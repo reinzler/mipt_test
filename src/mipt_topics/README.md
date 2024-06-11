fb7ba31b57e5423521a9e231881597095f6b01f6
vshtein <v.shtein@uniquerobotics.ru>
2024-06-11 11:56:36 +0300

    ROS_DOMAIN_ID

--- src/mipt_topics/README.md
+++ src/mipt_topics/README.md
@@ -7,3 +7,15 @@ ros2 topic pub /stop_cmd_park std_msgs/msg/Int8 "{data: 1}"
 ```
 ros2 topic pub /stop_cmd_park std_msgs/msg/Int8 "{data: 0}"
 ```
+```
+colcon build && source install/local_setup.bash
+ros2 launch mipt_topics topics.launch.py
+ros2 run mipt_topics mipt_topics.py --ros-args -r __ros_domain_id:=42
+```
+ROS_DOMAIN_ID
+```
+export ROS_DOMAIN_ID=42
+```
+```
+echo $ROS_DOMAIN_ID
+```
--- src/mipt_topics/launch/topics.launch.py
+++ src/mipt_topics/launch/topics.launch.py
@@ -1,9 +1,9 @@
 from launch_ros.actions import Node
 from launch import LaunchDescription
+from launch.actions import SetEnvironmentVariable
 
 
 def generate_launch_description():
-
     # Launch main emergency script
     main_script_launch = Node(package='mipt_topics',
                               executable='main_script',
@@ -11,4 +11,8 @@ def generate_launch_description():
                               # namespace='my_namespace',
                               output='screen')
 
+    # To launch the node in specific ROS_DOMAIN, for example in 42:
+    # return LaunchDescription([SetEnvironmentVariable('ROS_DOMAIN_ID', '42'), main_script_launch])
+
+    # Simply launch node in default ROS_DOMAIN (0)
     return LaunchDescription([main_script_launch])
